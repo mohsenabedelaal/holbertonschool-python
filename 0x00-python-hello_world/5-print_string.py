2a3b9d7a684a899701c27b33061e85546585aa58
Mohsen <mohsen.abedelaal@gmail.com>
2020-08-31 20:23:41 +0000

    printstr

--- 0x00-python-hello_world/5-print_string.py
+++ 0x00-python-hello_world/5-print_string.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
-str = "Holberton School"
-print("{0}{1}{2}".format(str,str,str))
+#!/usr/bin/python3 
+str = "Holberton School" 
+print("{}{}{}".format(str, str, str)) 
 print("{:.9}".format(str))

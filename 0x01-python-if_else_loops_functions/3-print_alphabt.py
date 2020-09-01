7b36a8261a8ecd661a9436e9ae9a6316a68592df
Mohsen <mohsen.abedelaal@gmail.com>
2020-09-01 10:34:13 +0000

    printalp

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 for i in range (97,123):     
-    if chr(i) == 'q' or chr(i) == 'e':         
-        continue     
-    print(chr(i),end="")
+    if chr(i) != 'q' or chr(i) != 'e':
+        print(chr(i),end="")

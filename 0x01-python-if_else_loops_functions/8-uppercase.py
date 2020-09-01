80665da66053e8c1d3dd9242be82fd0db0e6d582
Mohsen <mohsen.abedelaal@gmail.com>
2020-09-01 11:31:26 +0000

    uppercase

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def uppercase(str):
+    result=""
+    for i in str:
+        if ord(i) >= 97 and ord(i) <= 122:
+            y=chr(ord(i)-32)
+            result = result + y
+        else:
+            result = result + i

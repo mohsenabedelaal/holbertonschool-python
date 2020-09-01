fe904574d65f688f4050c2026d1710826a2b6374
Mohsen <mohsen.abedelaal@gmail.com>
2020-09-01 14:28:15 +0000

    ap

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 for i in range (0 , 99) :
     if i != 98 :
-        print("{0:0=2d}".format(i) , end = ", ")
+        print("{0:0=2d}".format(i), end = ", ")
     else :
-        print( i )
-    
+        print("{0:0=2d}".format(i), end="\n" )    

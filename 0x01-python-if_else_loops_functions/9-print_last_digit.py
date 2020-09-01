6dcda819cfa866f03a867af03dc5ea8edb6880d2
Mohsen <mohsen.abedelaal@gmail.com>
2020-09-01 11:48:39 +0000

    lastdig

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 def print_last_digit(number):
-    if number > 0:
-        print( number % 10 , end="")
-    else:
-        print( -1*(abs(number) % 10) , end="")
+    result = abs(number) % 10
+    print(result , end="")
+    return result

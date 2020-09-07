7d3f8dd320b5d92ecc894b07c9a33c0b78db182b
Mohsen <mohsen.abedelaal@gmail.com>
2020-09-07 11:22:02 +0000

    replinlist

---
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def replace_in_list(my_list, idx, element):
+    if idx > len(my_list) or idx < 0:
+        return None
+    else:
+        for i in range(0, len(my_list)):
+            if idx == i:
+                my_list[i] = element
+                return my_list
+        return None

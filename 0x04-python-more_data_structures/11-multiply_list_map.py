34f73c9d3839cd1132f90393809c330a92b55302
Mohsen <mohsen.abedelaal@gmail.com>
2020-09-08 18:50:23 +0000

    multlistmap

--- 0x04-python-more_data_structures/11-multiply_list_map.py
+++ 0x04-python-more_data_structures/11-multiply_list_map.py
@@ -1,7 +1,3 @@
 #!/usr/bin/python3
 def multiply_list_map(my_list=[], number=0):
-    if my_list and number is not None:
-        new = my_list[:]
-        new = list(map((lambda x: x*number), new))
-        return new
-    return my_list
+    return list(map((lambda x: x * number), my_list))

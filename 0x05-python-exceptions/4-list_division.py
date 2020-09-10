53b9c1abaaf0d5889de233599756f0449108f89f
Mohsen <mohsen.abedelaal@gmail.com>
2020-09-10 14:18:32 +0000

    safedivision

---
+++ 0x05-python-exceptions/4-list_division.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+def list_division(my_list_1, my_list_2, list_length):
+if list_length and my_list_1 and my_list_2:
+        new_list = [None] * list_length
+   for i in range(0, list_length):
+      try:
+                a = float(my_list_1[i])
+                b = float(my_list_2[i])
+                new_list[i] = a / b
+	  except ZeroDivisionError:
+                print("division by 0")
+	  except ValueError:
+                print("wrong type")
+	  except IndexError:
+                print("out of range")
+	  except:
+                pass
+		 finally:
+			 if new_list[i] is None:
+                    new_list[i] = 0
+			 else:
+                    new_list[i] = float(new_list[i])
+        return new_list

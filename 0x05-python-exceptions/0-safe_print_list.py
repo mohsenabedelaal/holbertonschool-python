059f84d84b87ace148c7cbd69651f2d9129c9a2f
Mohsen <mohsen.abedelaal@gmail.com>
2020-09-10 08:34:55 +0000

    safeprintlist

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    try:
+        count = 0
+        for i in range(0, x):
+            print(my_list[i], end="")
+            count = count + 1
+        print()
+        return x
+    except:
+        print()
+        return count

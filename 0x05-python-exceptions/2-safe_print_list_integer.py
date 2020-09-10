f72d622d45a6b474ccecaef982d006ef0358e45a
Mohsen <mohsen.abedelaal@gmail.com>
2020-09-10 09:07:26 +0000

    safeprintlist

---
+++ 0x05-python-exceptions/2-safe_print_list_integer.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    try:
+        count = 0
+        for i in range(0, x):
+            if (isinstance(my_list[i], int)):
+                print("{:d}".format(my_list[i]), end="")
+                count = count + 1
+        print()
+        return count
+    except IndexError as ex:
+        print(ex)

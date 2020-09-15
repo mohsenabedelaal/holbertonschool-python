cff9b083229318b32261fc1cd5f4ddf9c800cf10
Mohsen <mohsen.abedelaal@gmail.com>
2020-09-15 13:46:40 +0000

    first

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -10,7 +10,7 @@ class Square:
     """Square class.
        It defines a square and returns an empty block.
     """
-    def __init__(self, size = 0):
+    def __init__(self, size=0):
         if isinstance(size, int) and size > 0:
             self.size = size
         elif not isinstance(size, int):

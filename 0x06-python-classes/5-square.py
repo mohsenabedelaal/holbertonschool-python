06f083e8dbbc99fb802a97c41f1729424f9531ce
Mohsen <mohsen.abedelaal@gmail.com>
2020-09-15 15:50:04 +0000

    first

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -28,6 +28,10 @@ class Square():
         else:
             self.__size = value
 
-    def area(self):
-        """Function That Returns the square area of the Square"""
-        return self.__size * self.__size
+    def my_print(self):
+        """print out the square"""
+        if self.__size == 0:
+            print()
+        else:
+            for i in range(self.__size):
+                print("#" * self.__size)

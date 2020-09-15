23ba59bce1319b5488634b37b8a3ffa18c191dc8
Mohsen <mohsen.abedelaal@gmail.com>
2020-09-15 13:34:29 +0000

    first

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+    1-square.py
+    Module defining square
+    return {}
+"""
+
+
+class Square:
+    """Square class.
+       It defines a square and returns nothing.
+    """
+    def __init__(self,size):
+        self.__size = size

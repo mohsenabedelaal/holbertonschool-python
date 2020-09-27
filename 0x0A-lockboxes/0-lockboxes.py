8cfc424a0327e45a1ed32f2b7dab3887912fa83a
Mohsen <mohsen.abedelaal@gmail.com>
2020-09-27 16:48:45 +0000

    lockboxes

--- 0x0A-lockboxes/0-lockboxes.py
+++ 0x0A-lockboxes/0-lockboxes.py
@@ -3,12 +3,13 @@ def canUnlockAll(boxes):
     """Unlock the Boxes project """
     check_list = [False] * len(boxes)
     check_list[0] = True
-    for i in range(0, len(boxes)):
-        if isinstance(boxes[i], list):
-            for j in range(len(boxes[i])):
-                if boxes[i][j] <= len(boxes) and boxes[i][j] != i:
-                    check_list[boxes[i][j]] = True
-    if False in check_list:
-        return False
-    else:
-        return True
+    if isinstance(boxes, list):
+        for i in range(0, len(boxes)):
+            if isinstance(boxes[i], list):
+                for j in range(len(boxes[i])):
+                    if boxes[i][j] <= len(boxes) and boxes[i][j] != i:
+                        check_list[boxes[i][j]] = True
+        if False in check_list:
+            return False
+        else:
+            return True

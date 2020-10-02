338c836a1b1ae786711f2c1610f98663a2a0de0c
Mohsen <mohsen.abedelaal@gmail.com>
2020-10-02 17:11:20 +0000

    read

---
+++ 0x0C-python-input_output/5-to_json_string.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+Module 5-to_json_string.py
+"""
+
+
+import json
+
+
+def to_json_string(my_obj):
+        """write and append a text in a file"""
+        return (json.dumps(my_obj))

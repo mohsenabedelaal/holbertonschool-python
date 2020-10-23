3c10fd49d768d6a108f1b004a7f76d32719fb372
Mohsen <mohsen.abedelaal@gmail.com>
2020-10-23 16:34:27 +0000

    sqlad

---
+++ 0x13-SQL_more_queries/14-my_genres.sql
@@ -0,0 +1,2 @@
+-- MORE QUERY
+SELECT tv_genres.name FROM tv_show_genres INNER JOIN tv_shows ON tv_shows.id = tv_show_genres.show_id INNER JOIN tv_genres ON tv_genres.id = tv_show_genres.genre_id WHERE tv_shows.title = 'Dexter' ORDER BY tv_genres.name;
